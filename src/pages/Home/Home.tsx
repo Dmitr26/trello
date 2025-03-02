f5d9b8caf320fc36158200a2e01241a1402772ed
Dmitr26 <126329971+Dmitr26@users.noreply.github.com>
2025-03-02 16:06:00 +0200

    Task 2.2, Part 3-4
    I have set up all the API related data and also provided the backend connection in the file Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Home.scss';
 import { Board } from '../Home/components/Board/Board';
-
-const boardArray = [
-    { id: 1, title: "покупки", custom: { background: "red" } },
-    { id: 2, title: "підготовка до весілля", custom: { background: "green" } },
-    { id: 3, title: "розробка інтернет-магазину", custom: { background: "blue" } },
-    { id: 4, title: "курс по просуванню у соцмережах", custom: { background: "grey" } }
-]
+import { IBoard } from '../../common/interfaces/IBoard';
+import api from '../../api/request';
 
 export const Home = () => {
-    const [boards, setBoards] = useState(boardArray);
+    const [boards, setBoards] = useState<IBoard[]>([]);
+
+    const fetchData = async () => {
+        try {
+            const response = await api.get<IBoard[], { boards: IBoard[] }>('/board');
+            setBoards(response.boards);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     const boardComponents = boards.map((board) => <Board key={board.id} id={board.id} title={board.title} custom={board.custom} />);
 
